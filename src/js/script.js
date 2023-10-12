c80444b613d68daa5f000840c5ec9a4a20769c40
Dorota Maryniak <docia209aa@gmail.com>
2023-10-12 22:18:06 +0200

    Add filter with bug

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
 const list = document.querySelector('.books-list');
+const form = document.querySelector('.filters');
+
 
 function render(){
 
@@ -34,7 +36,28 @@ function initActions(){
 
   });
 
+  form.addEventListener('click', function(event){
+    console.log('form', form);
+    event.preventDefault();
+    console.log('event.target', event.target);
+
+    if (event.target.tagName=='INPUT'&&
+        event.target.type=='checkbox'&&
+        event.target.name=='filter') {
+      if (event.target.checked){
+        filters.push(event.target.value);
+        console.log('filters', filters);
+      }
+      else {
+        filters.splice(filters.indexOf(event.target.value),1);
+      }
+    }
+
+
+  });
 }
+
+const filters = [];
 
 
 render();
