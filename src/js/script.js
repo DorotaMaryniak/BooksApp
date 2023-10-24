c51ac37594030e750b4d7591c7cb574d918bd83c
Dorota Maryniak <docia209aa@gmail.com>
2023-10-24 21:47:13 +0200

    Add rating correct filter with bug

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,38 @@
 const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
 const list = document.querySelector('.books-list');
 const form = document.querySelector('.filters');
+const filters = [];
 
 
 function render(){
 
   for (let book in dataSource.books){
-    let generatedHTML= template(dataSource.books[book]);
+    let ratingBgc = determineRatingBgc(dataSource.books[book].rating);
+    let ratingWidth = dataSource.books[book].rating *10;
+    //console.log('ratingwidth', ratingWidth);
+
+    const allData= dataSource.books[book];
+    allData.ratingBgc = ratingBgc;
+    allData.ratingWidth = ratingWidth;
+
+    let generatedHTML= template(allData);
+
+    //console.log('generatedHTML', generatedHTML);
     let bookElement = utils.createDOMFromHTML(generatedHTML);
     list.appendChild(bookElement);
+    // console.log('bookelement', bookElement);
+
   }
 
 }
 const favoriteBooks =[];
 function initActions(){
-  console.log('lista', list);
+  // console.log('lista', list);
 
   list.addEventListener('dblclick', function(event){
     event.preventDefault();
     const clickedBook = event.target.offsetParent;
-    console.log('clickedbook', clickedBook);
+    // console.log('clickedbook', clickedBook);
     if(clickedBook.classList.contains('book__image')){
 
       const bookId = clickedBook.getAttribute('data-id');
@@ -37,9 +50,8 @@ function initActions(){
   });
 
   form.addEventListener('click', function(event){
-    console.log('form', form);
-    event.preventDefault();
-    console.log('event.target', event.target);
+  // console.log('form', form);
+  // console.log('event.target', event.target);
 
     if (event.target.tagName=='INPUT'&&
         event.target.type=='checkbox'&&
@@ -52,12 +64,45 @@ function initActions(){
         filters.splice(filters.indexOf(event.target.value),1);
       }
     }
+    filterBooks();
 
 
   });
 }
+function filterBooks () {
 
-const filters = [];
+
+  for (let book of dataSource.books){
+    let shouldBeHidden = false;
+    for (let filter of filters){
+      if (! book.details[filter]){
+        shouldBeHidden = true;
+      }
+    }
+    console.log('sould be hidden',shouldBeHidden);
+    const bookElement = document.querySelector('.book__image[data-id="' + book.id +'"]');
+    if (shouldBeHidden) {
+      bookElement.classList.add('hidden');
+    }
+    else{
+      bookElement.classList.remove('hidden');
+    }
+  }
+
+
+}
+
+function determineRatingBgc(rating){
+  if (rating <6)
+    return  'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+  else if (rating >6 && rating<=8)
+    return  ' linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+  else if (rating >8 && rating<=9)
+    return  '  linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+  else
+    return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+}
+
 
 
 render();
